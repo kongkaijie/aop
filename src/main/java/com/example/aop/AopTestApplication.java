7b99d899df6d52f06852d478bc613535caf42354
Kaijie <kaijie.kong@thoughtworks.com>
2023-07-28 08:40:33 +0800

    add comment

--- src/main/java/com/example/aop/AopTestApplication.java
+++ src/main/java/com/example/aop/AopTestApplication.java
@@ -8,17 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
-// 测试类
 @SpringBootApplication
 public class AopTestApplication {
     public static void main(String[] args) {
         // 创建目标对象
         UserService userService = new UserServiceImpl();
-
         // 创建InvocationHandler
         InvocationHandler handler = new LoggingInvocationHandler(userService);
 
-        // 第三步：创建代理对象，调用方法
+        // 第三步：创建代理对象，调用方法。
+        //生成的代理类会实现目标对象所实现的接口，并且在代理类的方法中调用 InvocationHandler 的 invoke() 方法来实现代理逻辑。
         UserService proxy = (UserService) Proxy.newProxyInstance(
                 userService.getClass().getClassLoader(),
                 userService.getClass().getInterfaces(),
