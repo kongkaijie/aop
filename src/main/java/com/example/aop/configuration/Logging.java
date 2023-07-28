29b2530efe42c04af9c0e33fcfd110717920dcff
Kaijie <kaijie.kong@thoughtworks.com>
2023-07-28 09:58:44 +0800

    add comment

--- src/main/java/com/example/aop/configuration/Logging.java
+++ src/main/java/com/example/aop/configuration/Logging.java
@@ -9,9 +9,9 @@ import java.lang.annotation.Target;
 //@Before("execution(public * com.itranswarp.learnjava.service.*.*(..))") public void check() {}
 
 //即某个包下面的所有方法都会被这个check()方法拦截。误伤概率很高。即很多不需要AOP代理的方法也被增强。
-// 并且，后续新增一些方法，如果不清楚现有的AOP装配规则，容易被强迫装配！
+// 并且，后续新增一些方法，如果我不清楚现有的AOP装配规则，容易被误装配
 
-//所以，在使用AOP时，为了让被装配的方法能清清楚楚地知道自己被安排了，推荐使用注解的方式来装配AOP===》既简单，又能明确标识AOP装配，时值得被推荐的。
+//所以，在使用AOP时，为了让被装配的方法能清清楚楚地知道自己被安排了，推荐使用注解的方式来装配AOP===》既简单，又能明确标识改方法被AOP装配了，是值得被推荐的。
 
 //第一步：定义logging的注解
 @Target(ElementType.METHOD)
