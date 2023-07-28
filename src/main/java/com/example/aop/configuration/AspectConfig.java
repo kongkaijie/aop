e51194b3c0398e42b2db2b89bff2c652dd077de2
Kaijie <kaijie.kong@thoughtworks.com>
2023-07-28 09:21:08 +0800

    add comment

--- src/main/java/com/example/aop/configuration/AspectConfig.java
+++ src/main/java/com/example/aop/configuration/AspectConfig.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.Import;
 @Configuration
 public class AspectConfig {
 
-    //集中管理切入点，方便维护和修改。
-    //将所有 @Pointcut 相关的配置写在一个类中，可以使得切入点的配置更加清晰明了，便于开发人员统一管理和修改。
-    //将不同 Pointcut 切点的业务逻辑单独作为一个类，可以将切面类分解成多个小类，每个小类只关注自己的业务逻辑，从而实现了切面类的解耦，提高了代码的可读性和可维护性。
+    //集中管理切入点
+    //将所有 @Pointcut 相关的配置写在一个类中，使得切入点的配置更加清晰明了，便于统一管理和修改。
+    //将不同 Pointcut 切点的业务逻辑单独作为一个类，可以将切面类分解成多个小类，每个小类只关注自己的业务逻辑，从而实现了切面类的解耦。
 
     @Pointcut("execution(* com.example.aop.controller.*.* (..))")
     public void generatedLog() {
